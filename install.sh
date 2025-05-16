8b7cdbf9ebd80350ef92bf132d3717f1b46a0c16
RWalecki <robert148@me.com>
2025-05-16 23:37:01 +0100

    Remove unused submodules and update installation script to clone necessary Vim plugins directly. Add new configuration files for Git, Vim, Tmux, and Zsh, while cleaning up old Zsh configuration files.

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 CONFIG_DIR=$HOME/.config  
 
-# Get the directory where the script file is located
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
 # Remove .config.old if it exists and move .config to .config.old
 if [ -d "$CONFIG_DIR" ]; then
   rm -rf "$CONFIG_DIR.old"
@@ -11,6 +8,7 @@ if [ -d "$CONFIG_DIR" ]; then
 fi
 
 # git clone --depth 1 git@github.com:RWalecki/config.git $CONFIG_DIR
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 cp -r $SCRIPT_DIR/config/ $CONFIG_DIR
 
 ln -sf $CONFIG_DIR/zsh/zshrc $HOME/.zshrc
@@ -22,3 +20,10 @@ git clone --depth 1 https://github.com/scrooloose/nerdtree.git $VIM_PACK_DIR/edi
 git clone --depth 1 https://github.com/vim-airline/vim-airline.git $VIM_PACK_DIR/editor/start/vim-airline.git
 git clone --depth 1 https://github.com/jistr/vim-nerdtree-tabs.git $VIM_PACK_DIR/editor/start/vim-nerdtree-tabs.git
 git clone --depth 1 https://github.com/tomasr/molokai.git $VIM_PACK_DIR/themes/start/molokai.git
+
+# install homebrew on macos
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  if [ ! -d "$HOME/.homebrew" ]; then
+    git clone --depth 1 https://github.com/Homebrew/brew.git $HOME/.homebrew
+  fi
+fi
