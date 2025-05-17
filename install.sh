8a7b58603a2fde9e51117e6b3d16ea9103d5fa2b
RWalecki <robert148@me.com>
2025-05-17 10:48:52 +0100

    Enhance installation script to prompt for custom config directory, manage existing Zsh and Git configuration files, and copy new dotfiles. Add symlinks for Git configuration and create necessary directories for Vim plugins. Update Zsh configuration to source from the new location.

--- install.sh
+++ install.sh
@@ -1,20 +1,28 @@
 #!/usr/bin/env bash
-CONFIG_DIR=$HOME/.config  
+# ask for config dir and default to $HOME/.config 
+read -p "Enter location of config dir [$HOME/.config] (Y to confirm, or enter custom path): " CONFIG_RESPONSE
+if [[ "$CONFIG_RESPONSE" == "Y" || "$CONFIG_RESPONSE" == "y" || "$CONFIG_RESPONSE" == "" ]]; then
+  CONFIG_DIR="$HOME/.config"
+else
+  CONFIG_DIR="$CONFIG_RESPONSE"
+fi
 
 # Remove .config.old if it exists and move .config to .config.old
-if [ -d "$CONFIG_DIR" ]; then
+if [ -d "$CONFIG_DIR" ] || [ -L "$CONFIG_DIR" ]; then
   rm -rf "$CONFIG_DIR.old"
   mv "$CONFIG_DIR" "$CONFIG_DIR.old"
 fi
 
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-cp -r $SCRIPT_DIR/config/ $CONFIG_DIR
+if [ -f "$HOME/.zshrc" ] || [ -L "$HOME/.zshrc" ]; then
+  mv "$HOME/.zshrc" "$HOME/.zshrc.old"
+fi
 
-ln -sf $CONFIG_DIR/zsh/zshrc $HOME/.zshrc
-ln -sf $CONFIG_DIR/git/gitconfig $HOME/.gitconfig
-ln -sf $CONFIG_DIR/git/gitignore $HOME/.gitignore
+# this has to be replaced with git clone
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+cp -r $SCRIPT_DIR/dotfiles $CONFIG_DIR
 
 export VIM_PACK_DIR=$CONFIG_DIR/vim/pack
+mkdir -p $VIM_PACK_DIR/{editor,themes}/start
 git clone --depth 1 https://github.com/Shougo/neocomplcache.vim.git $VIM_PACK_DIR/editor/start/neocomplcache.vim.git
 git clone --depth 1 https://github.com/scrooloose/nerdcommenter.git $VIM_PACK_DIR/editor/start/nerdcommenter.git
 git clone --depth 1 https://github.com/scrooloose/nerdtree.git $VIM_PACK_DIR/editor/start/nerdtree.git
@@ -28,6 +36,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     git clone --depth 1 https://github.com/Homebrew/brew.git $HOME/.homebrew
   fi
 fi
+
+touch $HOME/.zshrc
+cat << EOF >> "$HOME/.zshrc"
+export CONFIG_DIR=$CONFIG_DIR
+source $CONFIG_DIR/zshrc
+export VIMINIT='source $CONFIG_DIR/vim/vimrc'
+export TMUX_CONFIG=$CONFIG_DIR/tmux.conf
+EOF
 
 # start zsh
 zsh
